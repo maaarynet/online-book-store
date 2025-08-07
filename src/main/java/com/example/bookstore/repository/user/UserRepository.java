2e8b1e1b8db716227295bd7800a8f83caa97bdc0
maaarynet <mashajazz.mj@gmail.com>
2025-08-07 22:10:59 +0300

    Implemented spring security, added user roles

--- src/main/java/com/example/bookstore/repository/user/UserRepository.java
+++ src/main/java/com/example/bookstore/repository/user/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.bookstore.repository.user;
 
 import com.example.bookstore.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/example/bookstore/service/UserServiceImpl.java
+++ src/main/java/com/example/bookstore/service/UserServiceImpl.java
@@ -2,11 +2,17 @@ package com.example.bookstore.service;
 
 import com.example.bookstore.dto.user.UserRegistrationRequestDto;
 import com.example.bookstore.dto.user.UserResponseDto;
+import com.example.bookstore.exception.EntityNotFoundException;
 import com.example.bookstore.exception.RegistrationException;
 import com.example.bookstore.mapper.UserMapper;
+import com.example.bookstore.model.Role;
+import com.example.bookstore.model.RoleName;
 import com.example.bookstore.model.User;
+import com.example.bookstore.repository.role.RoleRepository;
 import com.example.bookstore.repository.user.UserRepository;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +20,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -23,6 +31,11 @@ public class UserServiceImpl implements UserService {
                     "User with this email already exists: " + requestDto.getEmail());
         }
         User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        Role userRole = roleRepository.findRoleByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find role for user: "
+                        + user.getUsername() + ", role: " + RoleName.ROLE_USER));
+        user.setRoles(Set.of(userRole));
         return userMapper.toResponseDto(userRepository.save(user));
     }
 }
